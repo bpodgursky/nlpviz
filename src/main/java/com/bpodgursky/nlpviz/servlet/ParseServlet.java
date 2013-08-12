1849aecdd72229b2fa44cec83df45f31a9b8a2e3
Ben Podgursky <bpodgursky@liveramp.com>
2013-08-12 02:03:00 -0700

    log

--- src/main/java/com/bpodgursky/nlpviz/servlet/ParseServlet.java
+++ src/main/java/com/bpodgursky/nlpviz/servlet/ParseServlet.java
@@ -2,6 +2,8 @@ package com.bpodgursky.nlpviz.servlet;
 
 import com.bpodgursky.nlpviz.ParseHelper2;
 import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ParseServlet extends HttpServlet {
+  private static final Logger LOG = LoggerFactory.getLogger(ParseServlet.class);
 
   private final ParseHelper2 helper;
 
@@ -22,6 +25,7 @@ public class ParseServlet extends HttpServlet {
 
     try {
       String sentence = req.getParameter("text");
+      LOG.info("Processing request: "+sentence);
 
       resp.getWriter().append(helper.parse(sentence).toString());
 
